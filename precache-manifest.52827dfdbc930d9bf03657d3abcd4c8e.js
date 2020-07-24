95c49ceef4b829627df9f9017f949326f8967af5
KyleMiles <krm504@nyu.edu>
2020-07-24 05:29:54 +0000

    Updates

---
+++ precache-manifest.52827dfdbc930d9bf03657d3abcd4c8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd30417f58ad46932d50f5a45adcf02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fa655f700fbc89a8d44",
+    "url": "/static/js/2.dc6a20cd.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.dc6a20cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2f5394730298968323",
+    "url": "/static/js/main.e44c4860.chunk.js"
+  },
+  {
+    "revision": "3a0cb5a2baa3a24c6bf9",
+    "url": "/static/js/runtime-main.0977cf3b.js"
+  }
+]);
